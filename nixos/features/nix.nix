84af7d725e39dd4fcd5e69e95e6eeb4152abb804
reingma <gabriel.masarin.aguiar@gmail.com>
2024-04-01 22:49:36 +0200

    Fixing nix settings

--- nixos/features/nix.nix
+++ nixos/features/nix.nix
@@ -5,12 +5,12 @@
 			experimental-features = "nix-command flakes";
 			auto-optimise-store = true;	
 			warn-dirty = false;
-			trusted-users = [ "root", "@wheel" ];
+			trusted-users = [ "root" "@wheel" ];
 		};
 		gc = {
 			automatic = true;
 			dates = "weekly";
-			options "--delete-older-than +5";
+			options = "--delete-older-than +5";
 		};
 		registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_:lib.isType "flake")) inputs);
 		nixPath = ["/etc/nix/path"];
