819f0988bee0ef7a12845aaabd9faa57f520b965
reingma <gabriel.masarin.aguiar@gmail.com>
2024-05-25 03:10:10 +0200

    Terminal Mode for nvim

--- home-manager/features/nvim/nvim_config/init.lua
+++ home-manager/features/nvim/nvim_config/init.lua
@@ -1,5 +1,6 @@
 require 'remaps'
 require 'opts'
+require 'terminal'
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
