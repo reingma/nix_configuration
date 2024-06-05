80396de0c47350a4e3b551adfc00d1715c837afb
reingma <gabriel.masarin.aguiar@gmail.com>
2024-06-05 11:21:10 +0200

    fix bug in remap

--- home-manager/features/nvim/nvim_config/lua/plugins/autoformat.lua
+++ home-manager/features/nvim/nvim_config/lua/plugins/autoformat.lua
@@ -12,7 +12,7 @@ return {
     },
   },
   opts = {
-    notify_on_error = false,
+    notify_on_error = true,
     format_on_save = function(bufnr)
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
@@ -26,7 +26,7 @@ return {
     formatters_by_ft = {
       lua = { 'stylua' },
       nix = { 'nixfmt' },
-      rust = { 'clippy' },
+      rust = { 'rustfmt' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
--- home-manager/features/nvim/nvim_config/lua/remaps.lua
+++ home-manager/features/nvim/nvim_config/lua/remaps.lua
@@ -27,7 +27,7 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move and ident up' })
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'n', 'Nzznzv')
+vim.keymap.set('n', 'N', 'Nzznzv')
 
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = '[F]ormat' })
 local set = vim.opt_local
