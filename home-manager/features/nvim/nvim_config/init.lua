c68eafe201a0cdd6c641cd2df6731c8f345090f4
reingma <gabriel.masarin.aguiar@gmail.com>
2024-09-15 15:14:59 +0200

    more nvim lsps

--- home-manager/features/nvim/nvim_config/init.lua
+++ home-manager/features/nvim/nvim_config/init.lua
@@ -52,6 +52,7 @@ require('lazy').setup({
   require 'plugins.vimtex',
   require 'plugins.haskell_tools',
   require 'plugins.tstools',
+  require 'plugins.metals',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
